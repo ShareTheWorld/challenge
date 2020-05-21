8bbe7969eade4a1c5c7b1cb61db1484f48528ac9
hongtao.fu <hongtao.fu@qq.com>
2020-05-22 06:17:57 +0800

    发现散列函数不行

--- filter/src/main/java/com/aliyun/filter/service/Page.java
+++ filter/src/main/java/com/aliyun/filter/service/Page.java
@@ -1,28 +1,44 @@
 package com.aliyun.filter.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Page {
     public byte[] data = new byte[32 * 1024 * 1024];//用于存放数据
     public static int min = 32 * 1024 * 1024 - 4028;//要求读数据的最小长度
     public int len;//用于存放数据的长度
     public int indexLen = 3;//索引使用字符的长度
-    public List<Log>[] bucket = new List[64 * 64 * 64];
+    public List<Log>[] bucket = new List[0X3FFFF];
     private static final int SKIP_LEN = 190;//跳过长度
 
     static long testTotal = 0;
     static long testErrorTotal = 0;
     static long testErrorTotal1 = 0;
     static long testErrorTotal2 = 0;
+    static int count = 0;
+    static int total = 0;
+    static int dicTotal = 0;
+    static Set<Integer> set = new HashSet<>();
+    static Set<String> set2 = new HashSet<>();
 
     //下面是建立索引的字段
     public void createIndex() {
         int i = 0;
+
         do {
 //            int index = get(data[i]) * get(data[i + 1]) * get(data[i + 2]);
-            int index = (get(data[i]) << 12) + (get(data[i + 1]) << 6) + get(data[i + 2]);
-            if (bucket[index] == null) bucket[index] = new ArrayList(20);
+//            int index = (get(data[i]) << 12) + (get(data[i + 1]) << 6) + get(data[i + 2]);
+            int index = ((data[i + 5] << 12) + (data[i + 7] << 9) + (data[i + 11] << 4) + data[i + 13]) & 0X3FFFF;
+//            int index = (get(data[i + 15]) * get(data[i + 8]) * get(data[i + 13]));
+            set2.add(new String(data, i, 15));
+            set.add(index);
+//            if (index > 65536) System.out.println(index);
+            if (index >= 0000 && index < 1) count++;
+            total++;
+//            if (i > 100) break;
+            if (bucket[index] == null) bucket[index] = new ArrayList(32);//平均大小17.5
             Log log = get(data, i);
             bucket[index].add(log);
             testTotal += log.len;
@@ -32,11 +48,16 @@ class Page {
 //        System.out.println("testErrorTotal=" + testErrorTotal);
 //        System.out.println("testErrorTotal1=" + testErrorTotal1);
 //        System.out.println("testErrorTotal2=" + testErrorTotal2);
+        System.out.println(count * 1.0 / total + "   " + count + "  " + total + "  " + set.size() + "  " + set2.size());
+//        for (int j = 0; j < bucket.length; j++) {
+//            if (bucket[j] != null) System.out.println(j + " = " + bucket[j].size());
+//            else System.out.println(j + " = " + 0);
+//        }
     }
 
     private int get(byte b) {
-        if (b >= 97) return b - 61;//b-97+36
-        if (b >= 65) return b - 55;//b-65+10
+        if (b >= 97) return b - 87;//b-97+10
+        //if (b >= 65) return b - 55;//b-65+190
         return b - 48;
     }
 
